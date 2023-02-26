b0cb21a22a9c8ff31c5aedefc876e1c5e9310e95
MinePlase <85606378+MinePlase@users.noreply.github.com>
2023-02-26 15:32:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,26 +3,13 @@ import string
 from random import choice
 from string import printable
 status = False
-score = int(input("Какова длинна пароля?"))
+score = int(input("Задайте длинну пароля в диапазоне от 7 до 20 (число): "))
 while score>20 or score < 7:
-    print("Пароль не подходит! Попробуй еще раз!")
+    print("К сожалению, пароль не входит в диапазон возможных значений. Попробуйте еще раз!")
     score = int(input("Какова длинна пароля?"))
 else:
     while status == False :
-                answer=input("Хотите получить пароль? (yes/no)  ")
-                if answer == "no":
-                    break
-                elif answer == "yes" :
-                    password = ''.join([choice(list(printable)) for item in range(0, score)])
-                    password2 = password.replace(" ", "")
-                    print(password2)
-                    status == True
-                    likedislike = input("Вам понравился пароль?  (yes/no)  ")
-                    while likedislike == "no":
-                        password = ''.join([choice(list(printable)) for item in range(0, score)])
-                        password2 = password.replace(" ", "")
-                        print(password2)
-                        likedislike = input("Вам понравился пароль?  (yes/no)  ")
-                    else:
-                        print("Спасибо за использование!")
+                    likedislike = input("Вам понравился пароль?  (да/нет): ")
+                    if likedislike == "да":
+                    print("Спасибо за использование")
                         break
